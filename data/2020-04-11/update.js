bdaf341be663e358ee87eb8e406b1500decdfdf7
Enrique Mireles <enrique.mireles@udem.edu>
2020-04-25 13:09:23 -0500

    add number of tests to files from 2020-04-11

--- data/2020-04-11/update.js
+++ data/2020-04-11/update.js
@@ -1,27 +1,22 @@
 const fs = require('fs');
+const utils = require('../../src/utils/utils');
 
-// const statsText = fs.readFileSync('./Stats.json', 'utf8');
-// const stats = JSON.parse(statsText);
-// stats.timeseries.forEach((s) => {
-//   s.active = 0;
-// });
-// stats.statesAsArray.forEach((s) => {
-//   s.active = 0;
-// });
-// Object.keys(stats.states).forEach((key) => {
-//   stats.states[key].active = 0;
-// });
-// fs.writeFileSync('./newStats.json', JSON.stringify(stats, null, 2), 'utf8');
+const statsText = fs.readFileSync('./Stats.json', 'utf8');
+const stats = JSON.parse(statsText);
+stats.timeseries.forEach((s) => {
+  s.tests = 0;
+});
+stats.statesAsArray.forEach((s) => {
+  s.tests = 0;
+});
+Object.keys(stats.states).forEach((key) => {
+  stats.states[key].tests = 0;
+});
+utils.saveJSON('./Stats.json', stats, false);
 
-// const statsByStateText = fs.readFileSync('./StatsByState.json', 'utf8');
-// const statsByState = JSON.parse(statsByStateText);
-// Object.keys(statsByState.states).forEach((key) => {
-//   statsByState.states[key].active = new Array(statsByState.dates.length).fill(
-//     0
-//   );
-// });
-// fs.writeFileSync(
-//   './newStatsByState.json',
-//   JSON.stringify(statsByState, null, 0),
-//   'utf8'
-// );
+const statsByStateText = fs.readFileSync('./StatsByState.json', 'utf8');
+const statsByState = JSON.parse(statsByStateText);
+Object.keys(statsByState.states).forEach((key) => {
+  statsByState.states[key].tests = new Array(statsByState.dates.length).fill(0);
+});
+utils.saveJSON('./StatsByState.json', statsByState, false);
