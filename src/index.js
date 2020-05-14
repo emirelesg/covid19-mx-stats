0054fed2e2c6b04ebffa5c954d66d1723213a8d3
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-14 16:42:47 -0500

    created a simple status server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const Service = require('./service');
+const Status = require('./status');
 
 const service = new Service();
+const status = new Status();
 
 console.clear();
+status.start();
 service.start();
 process.on('SIGINT', () => {
   console.log();
   service.stop();
+  status.stop();
   process.exit();
 });
---
+++ src/status.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const utils = require('./utils/utils');
+
+class Status {
+  constructor() {
+    this.log = utils.print.sectionFn('Status', 'magenta');
+    this.app = express();
+    this.app.get('/', (req, res) => {
+      const { timeseries } = utils.readJSON(utils.getLatestStats());
+      const latest = timeseries.slice(-1);
+      res.send(latest[0]);
+    });
+    this.server = undefined;
+  }
+
+  start() {
+    return new Promise((resolve) => {
+      this.server = this.app.listen(3000, () => {
+        this.log(`Started status server at port ${3000}`);
+        return resolve(true);
+      });
+    });
+  }
+
+  stop() {
+    if (this.server) {
+      this.server.close();
+      this.log(`Stoped status server`);
+    }
+  }
+}
+
+module.exports = Status;
