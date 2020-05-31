096d2208cf916ce2b0cb588ccdb4a7a11de6c80c
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-31 00:16:51 -0500

    redis monitoring

--- src/status.js
+++ src/status.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const redis = require('redis');
+const moment = require('moment');
 const utils = require('./utils/utils');
 
 class Status {
   constructor() {
     this.log = utils.print.sectionFn('Status', 'magenta');
+    this.redisClient = redis.createClient();
+    this.redisClient.on('connect', () =>
+      this.log(`Connected to redis server at ${this.redisClient.address}`)
+    );
     this.app = express();
     this.app.use(morgan('combined'));
     this.app.get('/', (req, res) => {
-      const { timeseries } = utils.readJSON(utils.getLatestStats());
-      const latest = timeseries.slice(-1);
-      res.send(latest[0]);
+      this.redisClient.hgetall('status', (err, obj) => {
+        if (err) {
+          res.status(500).send({ err });
+        } else {
+          res.send({ ...obj, heartbeat: moment(obj.heartbeat).fromNow() });
+        }
+      });
     });
     this.server = undefined;
     this.port = process.env.PORT || 3000;
@@ -28,7 +38,11 @@ class Status {
   stop() {
     if (this.server) {
       this.server.close();
-      this.log(`Stoped status server`);
+      this.log(`Stopped status server`);
+    }
+    if (this.redisClient.connected) {
+      this.redisClient.quit();
+      this.log(`Closed redis client`);
     }
   }
 }
@@ -36,5 +50,7 @@ class Status {
 const server = new Status();
 server.start();
 process.on('SIGINT', () => {
+  console.log();
   server.stop();
+  process.exit();
 });
--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,6 @@
 const schedule = require('node-schedule');
 const moment = require('moment');
+const redis = require('redis');
 const config = require('./config');
 const utils = require('./utils/utils');
 const tasks = require('./tasks');
@@ -8,10 +9,24 @@ class Service {
   constructor() {
     this.log = utils.print.sectionFn('Service', 'magenta');
     this.asService = !config.args.date;
+    this.redisClient = redis.createClient();
+    this.redisClient.on('connect', () => {
+      this.log(`Connected to redis server at ${this.redisClient.address}`);
+      if (this.asService) {
+        this.redisClient.del('status', () => {
+          this.redisClient.hset('status', 'isUpdating', 0);
+          this.redisClient.hset('status', 'isWaiting', 0);
+          this.redisClient.hset('status', 'heartbeat', '');
+          this.redisClient.hset('status', 'retries', 0);
+          this.redisClient.hset('status', 'lastUpdate', '');
+          this.redisClient.hset('status', 'remaining', '');
+        });
+      }
+    });
     this.today = undefined;
     this.yesterday = undefined;
     this.retries = 0;
-    this.mainJobInterval = '40 18 * * *';
+    this.mainJobInterval = '59 18 * * *';
     this.waitJobInterval = '* * * * * *';
     this.mainJob = schedule.scheduleJob(
       this.mainJobInterval,
@@ -30,6 +45,7 @@ class Service {
   // If any task failes, a countdown will start and the main worker
   // is started again.
   async onWork(completed) {
+    this.redisClient.hset('status', 'retries', this.retries);
     utils.print.welcome();
     utils.print.stats(this.today, this.retries);
     this.log('Started tasks');
@@ -69,18 +85,27 @@ class Service {
   // main worker finishes.
   async onSync() {
     this.stop();
+    this.redisClient.hset('status', 'isWaiting', '0');
+    this.redisClient.hset('status', 'isUpdating', '1');
     // Clear line after stoping wait job.
     if (this.asService) console.log();
     // Get dates.
     this.today = config.args.date ? moment(config.args.date) : moment();
     this.yesterday = moment(this.today).subtract(1, 'day');
     // Start work. Will only resolve on success.
+    this.retries = 0;
     await this.onWork();
+    this.redisClient.hset(
+      'status',
+      'lastUpdate',
+      this.today.format(config.outputDatePattern)
+    );
+    this.redisClient.hset('status', 'isUpdating', '0');
     if (this.asService) {
       // Restart everything again.
       this.start();
     } else {
-      this.stop();
+      this.exit();
     }
   }
 
@@ -89,6 +114,9 @@ class Service {
   onWait() {
     const { _date } = this.mainJob.nextInvocation();
     utils.print.wait(_date);
+    this.redisClient.hset('status', 'remaining', _date.fromNow());
+    this.redisClient.hset('status', 'heartbeat', moment().toISOString());
+    this.redisClient.hset('status', 'isWaiting', '1');
   }
 
   // Stops all jobs.
@@ -96,6 +124,17 @@ class Service {
     this.log(`Stopping cron jobs.`);
     this.waitJob.cancel();
     this.mainJob.cancel();
+  }
+
+  // Stops jobs and redis client.
+  exit() {
+    this.stop();
+    if (this.redisClient.connected) {
+      this.redisClient.hset('status', 'remaining', '', () => {
+        this.redisClient.quit();
+        this.log(`Closed redis client`);
+      });
+    }
   }
 
   // Starts all jobs. Sets the initial conditions.
@@ -118,6 +157,6 @@ console.clear();
 service.start();
 process.on('SIGINT', () => {
   console.log();
-  service.stop();
+  service.exit();
   process.exit();
 });
